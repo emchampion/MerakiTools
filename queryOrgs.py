d020e58050d411d88874a90d1cf795ebc921ba30
emchampion <EChampion@gmail.com>
2023-04-03 11:57:32 -0400

    New commit

--- queryOrgs.py
+++ queryOrgs.py
@@ -6,8 +6,6 @@ client = meraki.DashboardAPI(api_key=config.api_key)
 
 # Call the API to get the list of organizations
 orgs = client.organizations.getOrganizations()
-print(orgs)
-#{'id': '601230550253961288', 'name': 'Southeast Neighborhood Development', 'url': 'https://n68.meraki.com/o/V_oAVbeb/manage/organization/overview', 'api': {'enabled': True}, 'licensing': {'model': 'co-term'}, 'cloud': {'region': {'name': 'North America'}}
 
 # Print the list of organizations
 for org in orgs:
