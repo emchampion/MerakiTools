6ba6a5482c7ca8a19400a18f66dbe747859f1173
emchampion <EChampion@gmail.com>
2023-04-07 12:15:39 -0400

    New script

--- queryOrgs.py
+++ queryOrgs.py
@@ -6,7 +6,9 @@ client = meraki.DashboardAPI(api_key=config.api_key)
 
 # Call the API to get the list of organizations
 orgs = client.organizations.getOrganizations()
+#this is the pure json output, using for testing
+#print(orgs)
 
-# Print the list of organizations
+# Print the list of organizations Will use the raw JSON in the future
 for org in orgs:
     print(f'Name: {org["name"]}, ID: {org["id"]}')
